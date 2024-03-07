1efd67a3d2433486c8d7be9bfa44a6876f1528df
zerhmouti <139708431+zerhmouti@users.noreply.github.com>
2024-03-07 21:11:48 +0100

    Update PersonController.java
    test

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//test
+
 import com.example.model.Person;
 import com.example.service.PersonService;
 
